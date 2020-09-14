13f2b00eab20aa661db9181776b8a80ba7d6c153
dvlproad <dvlproad@163.com>
2020-09-14 18:18:07 +0800

    主页

--- tsdemodemo_flutter/lib/modules/main/main_page.dart
+++ tsdemodemo_flutter/lib/modules/main/main_page.dart
@@ -39,8 +39,8 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
