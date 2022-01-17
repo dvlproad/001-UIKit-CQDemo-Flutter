f3d01b73f31a6b17d923d5aeb4717c8c40ceb86f
dvlproad <dvlproad@163.com>
2022-01-17 22:40:09 +0800

    【Optimize】flutter_image_kit

--- flutter_image_kit/lib/src/image.dart
+++ flutter_image_kit/lib/src/image.dart
@@ -13,6 +13,18 @@ class TolerantNetworkImage extends StatelessWidget {
   final PlaceholderWidgetBuilder placeholder;
   final LoadingErrorWidgetBuilder errorWidget;
 
+  /// The duration of the fade-in animation for the [placeholder].
+  final Duration placeholderFadeInDuration;
+
+  /// The duration of the fade-out animation for the [placeholder].
+  final Duration fadeOutDuration;
+
+  /// The duration of the fade-in animation for the [imageUrl].
+  final Duration fadeInDuration;
+
+  /// Widget displayed while the target [imageUrl] is loading.
+  final ProgressIndicatorBuilder progressIndicatorBuilder;
+
   TolerantNetworkImage({
     this.width,
     this.height,
@@ -20,12 +32,15 @@ class TolerantNetworkImage extends StatelessWidget {
     this.fit = BoxFit.cover,
     this.placeholder,
     this.errorWidget,
+    this.placeholderFadeInDuration = Duration.zero,
+    this.fadeOutDuration = Duration.zero,
+    this.fadeInDuration = Duration.zero,
+    this.progressIndicatorBuilder,
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     bool urlValid = imageUrl != null && imageUrl.isEmpty == false;
     if (urlValid == true) {
       bool isNetworkUrl = imageUrl.startsWith(RegExp(r'https?:'));
@@ -39,13 +54,29 @@ class TolerantNetworkImage extends StatelessWidget {
         fit: fit,
         imageUrl: imageUrl,
         placeholder: placeholder,
-        errorWidget: errorWidget,
+        errorWidget: (context, url, error) {
+          if (this.errorWidget != null) {
+            return this.errorWidget(context, url, error);
+          } else {
+            return Container();
+          }
+        },
+        placeholderFadeInDuration: placeholderFadeInDuration,
+        fadeOutDuration: fadeOutDuration,
+        fadeInDuration: fadeInDuration,
+        progressIndicatorBuilder: (context, url, progress) {
+          if (this.progressIndicatorBuilder != null) {
+            return this.progressIndicatorBuilder(context, url, progress);
+          } else {
+            return Container(color: Color(0xFFF0F0F0));
+          }
+        },
       );
     } else {
       return Container(
         width: width,
         height: height,
-        color: Color(0xFFF2F2F2),
+        color: Color(0xFFF0F0F0),
       );
     }
   }
