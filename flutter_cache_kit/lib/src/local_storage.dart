3c3138a14917616fa72c6e0909b848726daf7fb9
dvlproad <dvlproad@163.com>
2022-01-18 17:55:05 +0800

    【Feature】添加自定义类的保存和获取

--- flutter_cache_kit/lib/src/local_storage.dart
+++ flutter_cache_kit/lib/src/local_storage.dart
@@ -1,4 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class LocalStorage {
   LocalStorage._();
@@ -9,8 +10,59 @@ class LocalStorage {
     prefs = await SharedPreferences.getInstance();
   }
 
+  static Future<bool> saveCustom(String key, value) async {
+    if (prefs == null) {
+      await LocalStorage.init(); // await 同步
+    }
+
+    if (value is List) {
+      List<String> strings = [];
+      for (var item in value) {
+        Map map = item.toJson();
+        String mapString = json.encode(map);
+        strings.add(mapString);
+      }
+      prefs.setStringList(key, strings);
+    } else {
+      Map map = value.toJson();
+      String mapString = json.encode(map);
+
+      return prefs.setString(key, mapString);
+    }
+  }
+
+  static getCustomBean<T>(String key, {T Function(Map bMap) fromJson}) {
+    String mapString = prefs.getString(key);
+    if (mapString == null) {
+      return null;
+    }
+
+    Map map = json.decode(mapString);
+    T customBean = fromJson(map);
+
+    return customBean;
+  }
+
+  static getCustomBeans<T>(String key, {T Function(Map bMap) fromJson}) {
+    List mapStrings = prefs.getStringList(key);
+    if (mapStrings == null) {
+      return null;
+    }
+
+    List<T> customBeans = [];
+    for (String mapString in mapStrings) {
+      Map map = json.decode(mapString);
+      T customBean = fromJson(map);
+      customBeans.add(customBean);
+    }
+    return customBeans;
+  }
+
   static Future<bool> save(String key, value) async {
-    if (value is Object == false) {}
+    if (prefs == null) {
+      await LocalStorage.init(); // await 同步
+    }
+
     if (value is String) {
       return prefs.setString(key, value);
     } else if (value is int) {
@@ -19,8 +71,12 @@ class LocalStorage {
       return prefs.setDouble(key, value);
     } else if (value is bool) {
       return prefs.setBool(key, value);
+    } else if (value is List) {
+      return prefs.setStringList(key, value);
+    } else {
+      Type type = value.runtimeType;
+      print('此类型${type.toString()}无法保存错误，请尝试使用saveCustomBean');
     }
-    return prefs.setStringList(key, value);
   }
 
   static Future<bool> cleanAll() async {
--- tsdemo_cache/lib/src/page/ts_cache_home_page.dart
+++ tsdemo_cache/lib/src/page/ts_cache_home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo_kit/flutter_demo_kit.dart';
-// import 'package:flutter_cache_kit/flutter_cache_kit.dart';
+import 'package:flutter_cache_kit/flutter_cache_kit.dart';
 
 class TSCacheHomePage extends StatefulWidget {
   @override
@@ -27,24 +27,186 @@ class _TSCacheHomePageState extends State<TSCacheHomePage> {
         child: ListView(
           children: <Widget>[
             ListTile(
-              title: Text('1'),
-              onTap: () {},
+              title: Text('保存 bool'),
+              onTap: () {
+                bool bValue = true;
+                LocalStorage.save('test_bool', bValue);
+                CJTSToastUtil.showMessage('保存结束');
+              },
             ),
             ListTile(
-              title: Text('2'),
-              onTap: () {},
+              title: Text('获取 bool'),
+              onTap: () {
+                bool bValue = LocalStorage.get('test_bool');
+                String message = bValue ? 'true' : 'false';
+                CJTSToastUtil.showMessage(message);
+              },
             ),
-            ListTile(
-              title: Text('3'),
-              onTap: () {},
+            rowWidget(
+              saveTitle: '保存 String',
+              onPressedSave: () {
+                String string =
+                    cqtsRandomString(3, 10, CQRipeStringType.english);
+                LocalStorage.save('test_string', string);
+                CJTSToastUtil.showMessage('保存结束');
+              },
+              getTitle: '获取 String',
+              onPressedGet: () {
+                String string = LocalStorage.get('test_string');
+                String message = string;
+                CJTSToastUtil.showMessage(message);
+              },
             ),
-            ListTile(
-              title: Text('4'),
-              onTap: () {},
+            rowWidget(
+              saveTitle: '保存 StringList',
+              onPressedSave: () {
+                List<String> stringList = [];
+                for (var i = 0; i < 10; i++) {
+                  String string =
+                      cqtsRandomString(3, 10, CQRipeStringType.english);
+                  stringList.add(string);
+                }
+                LocalStorage.save('test_stringList', stringList);
+                CJTSToastUtil.showMessage('保存结束');
+              },
+              getTitle: '获取 StringList',
+              onPressedGet: () {
+                List<String> stringList = LocalStorage.get('test_stringList');
+                String message = stringList.toString();
+                CJTSToastUtil.showMessage(message);
+              },
+            ),
+            rowWidget(
+              saveTitle: '保存自定义类 Custom',
+              onPressedSave: () {
+                TSCustomBean customBean = TSCustomBean(
+                  proxyId: '1001',
+                  proxyIp: '192.168.1.1',
+                  name: '代理',
+                );
+                LocalStorage.saveCustom('test_custom', customBean);
+                CJTSToastUtil.showMessage('保存结束');
+              },
+              getTitle: '获取自定义类 Custom',
+              onPressedGet: () {
+                TSCustomBean customBean = LocalStorage.getCustomBean(
+                  'test_custom',
+                  fromJson: (bMap) {
+                    return TSCustomBean.fromJson(bMap);
+                  },
+                );
+                String message = customBean.toString();
+                CJTSToastUtil.showMessage(message);
+              },
+            ),
+            rowWidget(
+              saveTitle: '保存自定义类 CustomBeans',
+              onPressedSave: () {
+                List<TSCustomBean> customBeans = [];
+                for (var i = 0; i < 10; i++) {
+                  TSCustomBean customBean = TSCustomBean(
+                    proxyId: '1001',
+                    proxyIp: '192.168.1.1',
+                    name: '代理',
+                  );
+                  customBeans.add(customBean);
+                }
+
+                LocalStorage.saveCustom('test_customBeans', customBeans);
+                CJTSToastUtil.showMessage('保存结束');
+              },
+              getTitle: '获取自定义类 CustomBeans',
+              onPressedGet: () {
+                List<TSCustomBean> customBeans = LocalStorage.getCustomBeans(
+                  'test_customBeans',
+                  fromJson: (bMap) {
+                    return TSCustomBean.fromJson(bMap);
+                  },
+                );
+                String message = customBeans.toString();
+                CJTSToastUtil.showMessage(message);
+              },
             ),
           ],
         ),
       ),
     );
+  }
+
+  // Widget rowWidget2(String key) {
+  //   return rowWidget(
+  //     saveTitle: '保存$key',
+  //     onPressedSave: () {
+  //       LocalStorage.save('test_bool', true);
+  //     },
+  //     getTitle: '获取$key',
+  //     onPressedGet: () {
+  //       bool bValue = LocalStorage.get('test_bool');
+  //       String message = bValue ? 'true' : 'false';
+  //       CJTSToastUtil.showMessage(message);
+  //     },
+  //   );
+  // }
+
+  Widget rowWidget(
+      {@required String saveTitle,
+      @required VoidCallback onPressedSave,
+      @required String getTitle,
+      @required VoidCallback onPressedGet}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CQTSThemeBGButton(
+          title: saveTitle,
+          onPressed: onPressedSave,
+        ),
+        CQTSThemeBGButton(
+          title: getTitle,
+          onPressed: onPressedGet,
+        ),
+      ],
+    );
+  }
+}
+
+class TSCustomBean {
+  String proxyId;
+  String name; // 网络代理的名称
+  String proxyIp; // 网络代理的 ip
+  String useDirection; // 使用说明
+  bool check; // 是否选中
+
+  TSCustomBean({
+    this.proxyId,
+    this.name,
+    this.proxyIp,
+    this.useDirection,
+    this.check,
+  });
+
+  // json 与 model 转换
+  factory TSCustomBean.fromJson(Map<String, dynamic> json) {
+    return TSCustomBean(
+      proxyId: json['proxyId'],
+      name: json['name'],
+      proxyIp: json['proxyIp'],
+      useDirection: json['useDirection'],
+      check: json['check'],
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      "proxyId": this.proxyId,
+      "name": this.name,
+      "proxyIp": this.proxyIp,
+      "useDirection": this.useDirection,
+      "check": this.check,
+    };
+  }
+
+  @override
+  String toString() {
+    return '{"proxyId":$proxyId, "proxyIp":$proxyIp, "name":$name}';
   }
 }
