3766b68da31de59714d4c8afc66e16ff3fce6d00
dvlproad <dvlproad@163.com>
2023-03-19 16:29:17 +0800

    【Fix】fix package

--- flutter_media_browser_interface/test/flutter_media_browser_interface_test.dart
+++ flutter_media_browser_interface/test/flutter_media_browser_interface_test.dart
@@ -1,12 +1,16 @@
+/*
+ * @Author: dvlproad
+ * @Date: 2023-03-18 16:30:38
+ * @LastEditors: dvlproad
+ * @LastEditTime: 2023-03-19 15:55:10
+ * @Description: 
+ */
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_media_browser_interface/flutter_media_browser_interface.dart';
+// import 'package:flutter_media_browser_interface/flutter_media_browser_interface.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    //
   });
 }
