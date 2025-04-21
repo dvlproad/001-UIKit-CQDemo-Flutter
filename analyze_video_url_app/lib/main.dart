adec41bf7f78f3a2779b33c5fc86c6c0c834d6cf
dvlproad <dvlproad@163.com>
2025-04-21 21:52:14 +0800

    flutter pub add firebase_crashlytics && flutter pub add firebase_analytics

--- analyze_video_url_app/lib/main.dart
+++ analyze_video_url_app/lib/main.dart
@@ -2,20 +2,95 @@
  * @Author: dvlproad
  * @Date: 2025-03-31 15:27:36
  * @LastEditors: dvlproad
- * @LastEditTime: 2025-04-19 01:33:33
+ * @LastEditTime: 2025-04-21 20:50:56
  * @Description: 
  */
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'home_page.dart';
 import 'l10n/l10n.dart';
 
-void main() {
+// Toggle this to cause an async error to be thrown during initialization
+// and to test that runZonedGuarded() catches the error
+const _kShouldTestAsyncErrorOnInit = false;
+
+// Toggle this for testing Crashlytics in your app locally.
+const _kTestingCrashlytics = true;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
+  const fatalError = true;
+  // Non-async exceptions
+  FlutterError.onError = (errorDetails) {
+    if (fatalError) {
+      // If you want to record a "fatal" exception
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+      // ignore: dead_code
+    } else {
+      // If you want to record a "non-fatal" exception
+      FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    }
+  };
+  // Async exceptions
+  // PlatformDispatcher.instance.onError = (error, stack) {
+  //   if (fatalError) {
+  //     // If you want to record a "fatal" exception
+  //     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  //     // ignore: dead_code
+  //   } else {
+  //     // If you want to record a "non-fatal" exception
+  //     FirebaseCrashlytics.instance.recordError(error, stack);
+  //   }
+  //   return true;
+  // };
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<void> _initializeFlutterFireFuture;
+
+  Future<void> _testAsyncErrorOnInit() async {
+    Future<void>.delayed(const Duration(seconds: 2), () {
+      final List<int> list = <int>[];
+      print(list[100]);
+    });
+  }
+
+  // Define an async function to initialize FlutterFire
+  Future<void> _initializeFlutterFire() async {
+    if (_kTestingCrashlytics) {
+      // Force enable crashlytics collection enabled if we're testing it.
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    } else {
+      // Else only enable it in non-debug builds.
+      // You could additionally extend this to allow users to opt-in.
+      // await FirebaseCrashlytics.instance
+      //     .setCrashlyticsCollectionEnabled(!kDebugMode);
+    }
+
+    if (_kShouldTestAsyncErrorOnInit) {
+      await _testAsyncErrorOnInit();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFlutterFireFuture = _initializeFlutterFire();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- analyze_video_url_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ analyze_video_url_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import app_settings
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
@@ -14,6 +17,9 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
